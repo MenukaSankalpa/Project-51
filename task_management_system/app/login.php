24fb90612cf6d47218d1a4f10ac05fd456993a1e
MenukaSankalpa <menuka.sankalpa@gmail.com>
2025-05-09 12:46:23 +0530

    after login made button

--- task_management_system/app/login.php
+++ task_management_system/app/login.php
@@ -1,9 +1,17 @@
 <?php
 
 if(isset($_POST['user_name']) && isset($_POST['password'])) {
+    function validate_input($data) {
+        $data = trim($data);
+        $data = stripsLashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
+    }
 
 } else {
-    
+    $em = "unknown error occurred";
+    $header("Location: ../login.php?error=$em");
+    exit();
 }
 
 
