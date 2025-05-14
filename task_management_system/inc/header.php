c0906eb7850cbab57d5387c8031f0a2ea19fb3df
MenukaSankalpa <menuka.sankalpa@gmail.com>
2025-05-14 16:38:15 +0530

    heder.php made and added it index.php

---
+++ task_management_system/inc/header.php
@@ -0,0 +1,8 @@
+<header class="header">
+    <h2 class="u-name">Task <b>Pro</b>
+        <label for="checkbox">
+            <i id="navbtn" class="ri-menu-line"></i>
+        </label>
+    </h2>
+    <i class="ri-user-fill"></i>
+</header>
