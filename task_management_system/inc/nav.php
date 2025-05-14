cd4a1d64835a99db46e918258a7080c263f12953
MenukaSankalpa <menuka.sankalpa@gmail.com>
2025-05-14 16:22:45 +0530

    nan.php file made and coded

---
+++ task_management_system/inc/nav.php
@@ -0,0 +1,85 @@
+<nav class="side-bar">
+            <div class="user-p">
+                <img src="img/user.jpg" alt="">
+                <h4>@user</h4>
+            </div>
+            <?php
+                $user = "admin";
+
+                if($user == "employee"){
+            ?>
+            <!-- Employee Navigation Bar-->
+            <ul>
+                <li>
+                    <a href="#">
+                        <i class="ri-macbook-fill"></i>
+                        <span>Dashboard</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <i class="ri-file-list-fill"></i>
+                        <span>My Task</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <i class="ri-message-2-fill"></i>
+                        <span>Profile</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <i class="ri-notification-3-fill"></i>
+                        <span>Notifications</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <i class="ri-logout-circle-fill"></i>
+                        <span>Logout</span>
+                    </a>
+                </li>
+            </ul>
+        <?php } else { ?>
+            <!-- Admin Navigation Bar-->
+            <ul>
+                <li>
+                    <a href="#">
+                        <i class="ri-macbook-fill"></i>
+                        <span>Dashboard</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <i class="ri-group-fill"></i>
+                        <span>Manage Users</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <i class="ri-add-circle-fill"></i>
+                        <span>Create Task</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <i class="ri-task-fill"></i>
+                        <span>All Task</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <i class="ri-notification-3-fill"></i>
+                        <span>Notifications</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <i class="ri-logout-circle-fill"></i>
+                        <span>Logout</span>
+                    </a>
+                </li>
+            </ul>
+        <?php } ?>
+        </nav>
