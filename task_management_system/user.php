08a26f5e07502f80828e357a21c72d1d56d98c25
MenukaSankalpa <menuka.sankalpa@gmail.com>
2025-05-15 16:35:57 +0530

    user.php file make and updated

---
+++ task_management_system/user.php
@@ -0,0 +1,34 @@
+<?php
+session_start();
+
+if (isset($_SESSION['role']) && isset($_SESSION['id'])) {
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Manage Users</title>
+    <link href="https://cdn.jsdelivr.net/npm/remixicon@4.5.0/fonts/remixicon.css" rel="stylesheet" />
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <input type="checkbox" name="" id="checkbox">
+    <?php include "inc/header.php"?>
+    <div class="body">
+        <?php include "inc/nav.php"?>
+        <section class="section-1">
+            <!--<h1>WELCOME</h1>
+            <P>#Project Task Management System</P>-->
+            Manage Users
+        </section>
+    </div>
+</body>
+</html>
+<?php } else {
+    $em = "first login";
+    header("Location: login.php?error=$em");
+    exit();
+}
+?>
