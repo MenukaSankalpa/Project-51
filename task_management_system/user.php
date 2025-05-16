615117c828563ed86e06221a8d9d78d9d2151e2d
MenukaSankalpa <menuka.sankalpa@gmail.com>
2025-05-16 11:48:05 +0530

    table design user.php file

--- task_management_system/user.php
+++ task_management_system/user.php
@@ -10,6 +10,8 @@ if (isset($_SESSION['role']) && isset($_SESSION['id'])) {
     <title>Manage Users</title>
     <link href="https://cdn.jsdelivr.net/npm/remixicon@4.5.0/fonts/remixicon.css" rel="stylesheet" />
     <link rel="stylesheet" href="css/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
 </head>
 <body>
     <input type="checkbox" name="" id="checkbox">
@@ -17,36 +19,24 @@ if (isset($_SESSION['role']) && isset($_SESSION['id'])) {
     <div class="body">
         <?php include "inc/nav.php"?>
         <section class="section-1">
-            Manage Users
-            <table class="table">
-                <thead>
-                  <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">First</th>
-                    <th scope="col">Last</th>
-                    <th scope="col">Handle</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <th scope="row">1</th>
-                    <td>Mark</td>
-                    <td>Otto</td>
-                    <td>@mdo</td>
-                  </tr>
-                  <tr>
-                    <th scope="row">2</th>
-                    <td>Jacob</td>
-                    <td>Thornton</td>
-                    <td>@fat</td>
-                  </tr>
-                  <tr>
-                    <th scope="row">3</th>
-                    <td>John</td>
-                    <td>Doe</td>
-                    <td>@social</td>
-                  </tr>
-                </tbody>
+            <h4 class="title">Manage Users <a href="add-user.php">Add User</a></h4>
+            <table class="main-table">
+                <tr>
+                    <th>#</th>
+                    <th>Full Name</th>
+                    <th>Username</th>
+                    <th>role</th>
+                    <th>Action</th>
+                </tr>
+                <tr><td>1</td>
+                    <td>Elias</td>
+                    <td>elias</td>
+                    <td>Employee</td>
+                    <td>
+                        <a href="" class="edit-btn" >Edit</a>
+                        <a href="" class="delete-btn" >Delete</a>
+                    </td>
+                </tr>
             </table>
         </section>
     </div>
