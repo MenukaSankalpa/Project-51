dc8d39c6a6f79655a062a4931c11d8eabacd2a14
MenukaSankalpa <menuka.sankalpa@gmail.com>
2025-05-19 16:56:43 +0530

    add-user.php file updated

---
+++ task_management_system/app/add-user.php
@@ -0,0 +1,81 @@
+<?php
+session_start();
+if(isset($_SESSION['role']) && isset($_SESSION['id'])){
+
+
+if(isset($_POST['user_name']) && isset($_POST['password']) &&  isset($_POST['full_name'])) {
+    include "../DB_connection.php";
+    function validate_input($data) {
+        $data = trim($data);
+        $data = stripsLashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
+    }
+
+    $user_name = validate_input($_POST['user_name']);
+    $password = validate_input($_POST['password']);
+    $full_name = validate_input($_POST['full_name']);
+
+    if (empty($user_name)) {
+        $em = "User Name Required";
+        header("Location: ../login.php?error=$em");
+        exit();
+    }else if (empty($password)) {
+        $em = "Password name is required";
+        header("Location: ../login.php?error=$em");
+        exit();
+    }else {
+
+        $sql = "SELECT * FROM users WHERE username = ?";
+        $stmt = $conn->prepare($sql);
+        $stmt->execute([$user_name]);
+
+        if ($stmt->rowCount() == 1) {
+            $user = $stmt->fetch();
+            $usernameDb = $user['username'];
+            $passwordDb = $user['password'];
+            $role = $user['role'];
+            $id = $user['id'];
+
+            if ($user_name === $usernameDb) {
+                if (password_verify($password, $passwordDb)) {
+                    if ($role == "admin") {
+                        $_SESSION['role'] = $role;
+                        $_SESSION['id'] = $id;
+                        $_SESSION['username'] = $usernameDb;
+                        header("Location: ../index.php");
+                    }elseif ($role == "employee") {
+                        $_SESSION['role'] = $role;
+                        $_SESSION['id'] = $id;
+                        $_SESSION['username'] = $usernameDb;
+                        header("Location: ../index.php");
+                    }else {
+                        $em = "Unknown error occurred";
+                        header("Location: ../login.php?error=$em");
+                        exit();
+                    }
+                }else {
+                    $em = "Incorrect username or password";
+                    header("Location: ../login.php?error=$em");
+                    exit();
+                }
+            }else {
+                $em = "Incorrect username or password";
+                header("Location: ../login.php?error=$em");
+                exit();
+            }
+        }
+    }
+} else {
+    $em = "Unknown error occurred";
+    header("Location: ../login.php?error=$em");
+    exit();
+}
+ 
+}else{
+    $em = "First login";
+    header("Location: ../login.php?error=$em");
+    exit();
+}
+
+?>
