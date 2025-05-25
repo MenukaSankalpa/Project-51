1306fde2f642b9062538fc52710e4b4f6f68a497
MenukaSankalpa <menuka.sankalpa@gmail.com>
2025-05-25 14:33:42 +0530

    updated error handler

--- task_management_system/app/Model/User.php
+++ task_management_system/app/Model/User.php
@@ -11,12 +11,14 @@ function get_all_users($conn){
     return  $users;
 }
 function insert_user($conn, $data){
-    $sql = "INSERT INTO users (full_name, username, password) VALUES(?,?,?)";
+    $sql = "INSERT INTO users (full_name, username, password, role) VALUES(?,?,?,?)";
     $stmt = $conn->prepare($sql);
     $stmt->execute($data);
-
+    
+    header("Location: ../add-user.php?success=User added successfully");
+    exit();
     /*if($stmt->rowCount() > 0){
-       $users = $stmt->fetchAll();
+    $users = $stmt->fetchAll();
     }else $users = 0;
 
     return  $users;*/
