931223a63da1c3c23368e1db0faa5abec97f91d0
MenukaSankalpa <menuka.sankalpa@gmail.com>
2025-05-26 15:58:14 +0530

    updated-user.php file updated

---
+++ task_management_system/app/update-user.php
@@ -0,0 +1,56 @@
+<?php
+session_start();
+if(isset($_SESSION['role']) && isset($_SESSION['id'])){
+
+
+if(isset($_POST['user_name']) && isset($_POST['password']) &&  isset($_POST['full_name'])) {
+    include "../DB_connection.php";
+    function validate_input($data) {
+        $data = trim($data);
+        $data = stripsLashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
+    }
+
+    $user_name = validate_input($_POST['user_name']);
+    $password = validate_input($_POST['password']);
+    $full_name = validate_input($_POST['full_name']);
+    $id = validate_input($_POST['id']);
+
+    if (empty($user_name)) {
+        $em = "User Name is Required";
+        header("Location: ../edit-user.php?error=$em$id=$id");
+        exit();
+    }else if (empty($password)) {
+        $em = "Password is required";
+        header("Location: ../edit-user.php?error=$em$id=$id");
+        exit();
+    }else if (empty($full_name)) {
+        $em = "Full name is required";
+        header("Location: ../edit-user.php?error=$em$id=$id");
+        exit();
+    }else {
+
+        /*$sql = "INSERT INTO * FROM users WHERE username = ?";
+        $stmt = $conn->prepare($sql);
+        $stmt->execute([$user_name]);*/
+
+        include "Model/User.php";
+        $password = password_hash($password, PASSWORD_DEFAULT);
+        $data = array($full_name, $user_name, $password, "employee");
+        insert_user($conn, $data);
+        
+        $em = "User created successfully";
+        header("Location: ../edit-user.php?error=$em$id=$id");
+        exit();
+    }
+} else {
+    $em = "First login";
+    header("Location: ../edit-user.php?error=$em$id=$id");
+    exit();
+}
+}else{
+    $em= "First login";
+    header("Location: ../edit-user.php?error=$em");
+    exit();
+}
