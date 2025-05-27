8b5d7574c095110892063187afa277d56e1b2a81
MenukaSankalpa <menuka.sankalpa@gmail.com>
2025-05-27 08:49:38 +0530

    User.php file updated

--- task_management_system/app/Model/User.php
+++ task_management_system/app/Model/User.php
@@ -25,7 +25,7 @@ function insert_user($conn, $data){
 }
 
 function update_user($conn, $data){
-    $sql = "INSERT INTO users (full_name, username, password, role) VALUES(?,?,?,?)";
+    $sql = "UPDATE users SET  full_name=?, username=?, password=?, role=?) WHERE id=? AND role=?";
     $stmt = $conn->prepare($sql);
     $stmt->execute($data);
 }
