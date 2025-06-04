a9ec3735b097a7a80b1dca5534743aef46c9db69
MenukaSankalpa <menuka.sankalpa@gmail.com>
2025-06-04 14:31:08 +0530

    create_task.php file made

---
+++ task_management_system/create_task.php
@@ -0,0 +1,41 @@
+<?php
+session_start();
+if (isset($_SESSION['role']) && isset($_SESSION['id'])) {
+    include "DB_connection.php";
+    include "app/Model/User.php";
+    $users = get_all_users($conn);
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Create Task</title>
+    <link href="https://cdn.jsdelivr.net/npm/remixicon@4.5.0/fonts/remixicon.css" rel="stylesheet" />
+    <link rel="stylesheet" href="css/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
+</head>
+<body>
+    <input type="checkbox" name="" id="checkbox">
+    <?php include "inc/header.php"?>
+    <div class="body">
+        <?php include "inc/nav.php"?>
+        <section class="section-1">
+            <h4 class="title">Create Task</h4>
+        </section>
+    </div>
+
+<script type="text/javascript">
+    var active = document.querySelector("#navList li:nth-child(3)");
+    active.classList.add("active");
+</script>
+
+</body>
+</html>
+<?php } else {
+    $em = "First login";
+    header("Location: login.php?error=$em");
+    exit();
+}
+?>
