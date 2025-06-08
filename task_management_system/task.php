f3b78937926bc3a8ecbe7caa1cd527319cc2fe98
MenukaSankalpa <menuka.sankalpa@gmail.com>
2025-06-08 09:15:22 +0530

    task.php added delete button and edit button path

--- task_management_system/task.php
+++ task_management_system/task.php
@@ -44,8 +44,8 @@ if (isset($_SESSION['role']) && isset($_SESSION['id'])) {
                     <td><?=$task['description']?></td>
                     <td><?=$task['assigned_to']?></td>
                     <td>
-                        <a href="edit-user.php?id=<?=$user['id']?>" class="edit-btn" >Edit</a>
-                        <a href="delete-user.php?id=<?=$user['id']?>" class="delete-btn" >Delete</a>
+                        <a href="edit-task.php?id=<?=$task['id']?>" class="edit-btn" >Edit</a>
+                        <a href="delete-task.php?id=<?=$task['id']?>" class="delete-btn" >Delete</a>
                     </td>
                 </tr>
                 <?php } ?>
