1de17d0f018c71ba641bacc537680ff6ba1c5cdb
MenukaSankalpa <menuka.sankalpa@gmail.com>
2025-06-13 16:49:56 +0530

    update-profile.php file updated

--- task_management_system/app/update-profile.php
+++ task_management_system/app/update-profile.php
@@ -3,7 +3,7 @@ session_start();
 if(isset($_SESSION['role']) && isset($_SESSION['id'])){
 
 
-if(isset($_POST['user_name']) && isset($_POST['password']) &&  isset($_POST['full_name']) && $_SESSION['role'] == 'employee') {
+if(isset($_POST['confirm_password']) && isset($_POST['new_password']) && isset($_POST['password']) &&  isset($_POST['full_name']) && $_SESSION['role'] == 'employee') {
     include "../DB_connection.php";
     function validate_input($data) {
         $data = trim($data);
@@ -12,22 +12,26 @@ if(isset($_POST['user_name']) && isset($_POST['password']) &&  isset($_POST['ful
         return $data;
     }
 
-    $user_name = validate_input($_POST['user_name']);
     $password = validate_input($_POST['password']);
     $full_name = validate_input($_POST['full_name']);
-    $id = validate_input($_POST['id']);
+    $new_password = validate_input($_POST['new_password']);
+    $confirm_password = validate_input($_POST['confirm_password']);
 
     if (empty($user_name)) {
         $em = "User Name is Required";
-        header("Location: ../edit-user.php?error=$em&id=$id");
+        header("Location: ../edit_profile.php?error=$em");
         exit();
-    }else if (empty($password)) {
+    }else if (empty($password) || empty($new_password) || empty($confirm_password)) {
         $em = "Password is required";
-        header("Location: ../edit-user.php?error=$em&id=$id");
+        header("Location: ../edit_profile.php?error=$em");
         exit();
     }else if (empty($full_name)) {
         $em = "Full name is required";
-        header("Location: ../edit-user.php?error=$em&id=$id");
+        header("Location: ../edit_profile.php?error=$em");
+        exit();
+    }else if (empty($confirm_password != $new_password)) {
+        $em = "New password and confirm password do not match! ";
+        header("Location: ../edit_profile.php?error=$em");
         exit();
     }else {
 
@@ -36,21 +40,37 @@ if(isset($_POST['user_name']) && isset($_POST['password']) &&  isset($_POST['ful
         $stmt->execute([$user_name]);*/
 
         include "Model/User.php";
-        $password = password_hash($password, PASSWORD_DEFAULT);
-        $data = array($full_name, $user_name, $password, "employee", $id, "employee");
-        update_user($conn, $data);
-        
-        $em = "User created successfully";
-        header("Location: ../edit-user.php?error=$em&id=$id");
-        exit();
+
+        $user = get_user_by_id($co);
+        if ($user){
+            if (password_verify($password, $user['password'])) {
+            
+            $new_password = password_hash($new_password, PASSWORD_DEFAULT);
+
+            $data = array($full_name, $new_password);
+            update_profile($conn, $data);
+            
+            $em = "User created successfully";
+            header("Location: ../edit_profile.php?error=$em");
+            exit();
+            } else {
+                $em = "Incorrect password! ";
+                header("Location: ../edit_profile.php?error=$em");
+                exit();
+            }
+        }else {
+                $em = "Unknown error occurred ";
+                header("Location: ../edit_profile.php?error=$em");
+                exit();
+        }
     }
 } else {
-    $em = "First login";
-    header("Location: ../edit-user.php?error=$em");
+    $em = "Unknown error occurred";
+    header("Location: ../edit_profile.php?error=$em");
     exit();
 }
 }else{
     $em= "First login";
-    header("Location: ../edit-user.php?error=$em");
+    header("Location: ../login.php?error=$em");
     exit();
 }
