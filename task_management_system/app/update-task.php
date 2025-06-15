67840ff17b330b0a3b6e98e087f7c95360d78b39
MenukaSankalpa <menuka.sankalpa@gmail.com>
2025-06-15 15:40:07 +0530

    update_task.php file updated

--- task_management_system/app/update-task.php
+++ task_management_system/app/update-task.php
@@ -3,7 +3,8 @@ session_start();
 if(isset($_SESSION['role']) && isset($_SESSION['id'])){
 
 
-if(isset($_POST['id']) && isset($_POST['title']) && isset($_POST['description']) &&  isset($_POST['assigned_to']) && $_SESSION['role'] == 'admin' ) {
+if  (isset($_POST['id']) && isset($_POST['title']) && isset($_POST['description'])
+    &&  isset($_POST['assigned_to']) && $_SESSION['role'] == 'admin' && isset($_POST['due_date'])) {
     include "../DB_connection.php";
     function validate_input($data) {
         $data = trim($data);
@@ -16,6 +17,7 @@ if(isset($_POST['id']) && isset($_POST['title']) && isset($_POST['description'])
     $description = validate_input($_POST['description']);
     $assigned_to = validate_input($_POST['assigned_to']);
     $id = validate_input($_POST['id']);
+    $due_date = validate_input($_POST['due_date']);
 
     if (empty($title)) {
         $em = "Title is Required";
@@ -38,7 +40,7 @@ if(isset($_POST['id']) && isset($_POST['title']) && isset($_POST['description'])
 
         include "Model/Task.php";
         //$password = password_hash($password, PASSWORD_DEFAULT);
-        $data = array($title, $description, $assigned_to, $id);
+        $data = array($title, $description, $assigned_to, $due_date, $id);
         update_task($conn, $data);
         
         $em = "Task updated successfully";
