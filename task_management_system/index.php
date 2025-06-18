0b3a679fc0ad26f28ac9d87ccf36dac743a54329
MenukaSankalpa <menuka.sankalpa@gmail.com>
2025-06-18 16:01:21 +0530

    function names called

--- task_management_system/index.php
+++ task_management_system/index.php
@@ -6,14 +6,23 @@ if (isset($_SESSION['role']) && isset($_SESSION['id'])) {
     include "app/Model/Task.php";
     include "app/Model/User.php";
 
-    $todaydue_task = count_tasks_due_today($conn);
-    $overdue_task = count_tasks_overdue($conn);
-    $nodeadline_task = count_tasks_NoDeadline($conn);
-    $num_task = count_tasks($conn);
-    $num_users = count_users($conn);
-    $pending = count_pending_tasks($conn);
-    $in_progress = count_in_progress_tasks($conn);
-    $completed = count_completed_tasks($conn);
+    if ($_SESSION['role'] == "admin"){
+        $todaydue_task = count_tasks_due_today($conn);
+        $overdue_task = count_tasks_overdue($conn);
+        $nodeadline_task = count_tasks_NoDeadline($conn);
+        $num_task = count_tasks($conn);
+        $num_users = count_users($conn);
+        $pending = count_pending_tasks($conn);
+        $in_progress = count_in_progress_tasks($conn);
+        $completed = count_completed_tasks($conn);
+    } else {
+        $num_my_task = count_my_tasks($conn, $_SESSION['id']);
+        $overdue_task = count_my_tasks_overdue($conn, $_SESSION['id']);
+        $nodeadline_task = count_my_tasks_NoDeadline($conn, $_SESSION['id']);
+        $pending = count_my_pending_tasks($conn, $_SESSION['id']);
+        $in_progress = count_my_in_progress_tasks($conn, $_SESSION['id']);
+        $completed = count_my_completed_tasks($conn, $_SESSION['id']);
+    }
 
 
 ?>
@@ -58,6 +67,33 @@ if (isset($_SESSION['role']) && isset($_SESSION['id'])) {
                     <div class="dashboard-item">
                         <i class="ri-notification-fill"></i>
                         <span><?=$overdue_task?> Notifications</span>
+                    </div>
+                    <div class="dashboard-item">
+                        <i class="ri-pass-pending-fill"></i>
+                        <span><?=$pending?> Pending</span>
+                    </div>
+                    <div class="dashboard-item">
+                        <i class="ri-loop-left-fill"></i>
+                        <span><?=$in_progress?> In Progress</span>
+                    </div>
+                    <div class="dashboard-item">
+                        <i class="ri-check-double-fill"></i>
+                        <span><?=$completed?> Completed</span>
+                    </div>
+                </div>
+            <?php }else{?>
+                <div class="dashboard">
+                    <div class="dashboard-item">
+                        <i class="ri-task-fill"></i>
+                        <span><?=$num_my_task?> My Tasks</span>
+                    </div>
+                    <div class="dashboard-item">
+                        <i class="ri-close-fill"></i>
+                        <span><?=$overdue_task?> Overdue</span>
+                    </div>
+                    <div class="dashboard-item">
+                        <i class="ri-time-fill"></i>
+                        <span><?=$nodeadline_task?> No Dead Line</span>
                     </div>
                     <div class="dashboard-item">
                         <i class="ri-pass-pending-fill"></i>
