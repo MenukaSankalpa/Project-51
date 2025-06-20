700e42ec8fc0206bac400ce6bc79258be9b6eb2b
MenukaSankalpa <menuka.sankalpa@gmail.com>
2025-06-20 10:19:34 +0530

    notification-bar class made

--- task_management_system/inc/header.php
+++ task_management_system/inc/header.php
@@ -4,5 +4,19 @@
             <i id="navbtn" class="ri-menu-line"></i>
         </label>
     </h2>
-    <i class="ri-user-fill"></i>
+    <span class="notification">
+        <i class="ri-user-fill" aria-hidden="true"></i>
+        <span>&nbsp;7&nbsp;</span>
+    </span>
 </header>
+<div class="notification-bar">
+    <ul>
+        <li>
+            <a href="">
+                <mark>New Task Assigned:</mark> 'Customer Feedback Survey Analysis' has been assigned to you. Please review 
+                and start working on it
+                &nbsp;&nbsp;<small>Sep 2, 2024</small>
+            </a>
+        </li>
+    </ul>
+</div>
